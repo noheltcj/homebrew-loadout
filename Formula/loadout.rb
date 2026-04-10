1eb22207709aba88e2a0a26344a0b7f1d539d8e7
Colton Nohelty <noheltycolton@gmail.com>
2026-04-10 04:34:11 -0500

    Update formula to version 0.5.0 (#11)

--- Formula/loadout.rb
+++ Formula/loadout.rb
@@ -1,26 +1,26 @@
 class Loadout < Formula
   desc "CLI tool for managing and composing configuration fragments across projects"
   homepage "https://github.com/noheltcj/loadout"
-  version "0.4.0"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/noheltcj/loadout/releases/download/v0.4.0/loadout-macos-arm64.tar.gz"
-      sha256 "49d17ec616988f2913e60de6b699bc16105172573c22e642562d217ae4c7af87"
+      url "https://github.com/noheltcj/loadout/releases/download/v0.5.0/loadout-macos-arm64.tar.gz"
+      sha256 "44f52d1d871676caf1ab28853aa5c01e53b27da5c6d9406d1f721cff93d48fcd"
     else
-      url "https://github.com/noheltcj/loadout/releases/download/v0.4.0/loadout-macos-x64.tar.gz"
-      sha256 "49cb69143a9a82478d0b7f3b7bdec19fd0a642a05f382043a768f7f319211d28"
+      url "https://github.com/noheltcj/loadout/releases/download/v0.5.0/loadout-macos-x64.tar.gz"
+      sha256 "475a5e1c06336c4f876beabacc8220e0be8382e64ebda82613aa6ea5018ec230"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/noheltcj/loadout/releases/download/v0.4.0/loadout-linux-arm64.tar.gz"
-      sha256 "aaee4ab63be9fc64320796f663222376098d90b434f38d9b116208d62dd2a3f4"
+      url "https://github.com/noheltcj/loadout/releases/download/v0.5.0/loadout-linux-arm64.tar.gz"
+      sha256 "e07460d6286c2ca8121ac6a4a7709c46e58287f21032e70f4a3e8aa92fc25f1a"
     else
-      url "https://github.com/noheltcj/loadout/releases/download/v0.4.0/loadout-linux-x64.tar.gz"
-      sha256 "f06928ff80205dd82ef950f79e003ba4698a5bb048c2390a06c494e8a30c75d2"
+      url "https://github.com/noheltcj/loadout/releases/download/v0.5.0/loadout-linux-x64.tar.gz"
+      sha256 "25ea08d316455c695541fc826a695f5f7b2a7961c616a9fc50fb1aca8dc5a513"
     end
   end
 
